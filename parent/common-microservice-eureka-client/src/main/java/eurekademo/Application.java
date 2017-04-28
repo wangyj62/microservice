e51e53651a2537c5407eadf9b4ff4d8879eb95a6
Administrator <Administrator@QH-20170321YPWM>
2017-04-28 10:38:06 +0800

    123

---
+++ parent/common-microservice-eureka-client/src/main/java/eurekademo/Application.java
@@ -0,0 +1,25 @@
+package eurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "This is a trivial service that demonstrates how a Eureka Client can register with a Eureka Server";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
--- parent/common-microservice-eureka/src/main/java/eurekademo/EurekaApplication.java
+++ parent/common-microservice-eureka/src/main/java/eurekademo/EurekaApplication.java
@@ -1,32 +1,15 @@
 package eurekademo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author Gunnar Hillert
- *
- */
-@Configuration
-@EnableAutoConfiguration
-@EnableEurekaClient
-@RestController
+@SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	@RequestMapping("/")
-	  public String home() {
-	    return "Hello World";
-	  }
-
-	  public static void main(String[] args) {
-	    SpringApplication.run(EurekaApplication.class, args);
-	  }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
 
 }
